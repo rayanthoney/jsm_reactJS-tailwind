4aa8ab5a8a6085ce95953b8472f99e5e8709d70b
Ray Anthoney <rmaxey911@gmail.com>
2023-12-05 08:56:17 -0700

    commit index

--- jsm_react-tailwind-socialmedia/src/lib/validation/index.ts
+++ jsm_react-tailwind-socialmedia/src/lib/validation/index.ts
@@ -9,3 +9,8 @@ import * as z from "zod";
     email: z.string().email(),
     password: z.string().min(8, { message: 'Password must be at least 8 characters.' }),
   });
+ 
+  export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, { message: 'Password must be at least 8 characters.' }),
+  });
