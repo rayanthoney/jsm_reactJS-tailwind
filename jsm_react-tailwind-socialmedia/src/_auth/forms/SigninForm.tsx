e2ad95634d84879329ee4f0f4ab81d37d405aeca
Ray Anthoney <rmaxey911@gmail.com>
2023-12-08 05:29:31 -0700

    commit signup

--- jsm_react-tailwind-socialmedia/src/_auth/forms/SigninForm.tsx
+++ jsm_react-tailwind-socialmedia/src/_auth/forms/SigninForm.tsx
@@ -17,7 +17,7 @@ const SigninForm = () => {
   const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
   const navigate = useNavigate();
   
-  const { mutateAsync: signInAccount, } = useSignInAccount();
+  const { mutateAsync: signInAccount } = useSignInAccount();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SigninValidation>>({
