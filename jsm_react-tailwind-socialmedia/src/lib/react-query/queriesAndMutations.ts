c1ff8e711fe924cd099803313fa05a5809297ca7
Ray Anthoney <rmaxey911@gmail.com>
2023-12-16 19:37:52 -0700

    added queries

--- jsm_react-tailwind-socialmedia/src/lib/react-query/queriesAndMutations.ts
+++ jsm_react-tailwind-socialmedia/src/lib/react-query/queriesAndMutations.ts
@@ -1,8 +1,8 @@
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  useInfiniteQuery,
+import { 
+  useQuery, 
+  useMutation, 
+  useQueryClient, 
+  useInfiniteQuery, 
 } from "@tanstack/react-query";
 import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
 import { INewUser } from "@/types";
@@ -23,5 +23,17 @@ export const useSignInAccount = () => {
 export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: signOutAccount,
+  });
+};
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
   });
 };
