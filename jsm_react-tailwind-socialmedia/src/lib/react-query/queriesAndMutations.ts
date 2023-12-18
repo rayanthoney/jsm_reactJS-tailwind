bdd239456b9591d4b2c9d43abf2c04400211af2b
Ray Anthoney <rmaxey911@gmail.com>
2023-12-17 18:43:15 -0700

    added useCreatePost

--- jsm_react-tailwind-socialmedia/src/lib/react-query/queriesAndMutations.ts
+++ jsm_react-tailwind-socialmedia/src/lib/react-query/queriesAndMutations.ts
@@ -4,8 +4,8 @@ import {
   useQueryClient, 
   useInfiniteQuery, 
 } from "@tanstack/react-query";
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import { createPost, createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
+import { INewPost, INewUser } from "@/types";
 
 export const useCreateUserAccount = () => {
   return useMutation({
