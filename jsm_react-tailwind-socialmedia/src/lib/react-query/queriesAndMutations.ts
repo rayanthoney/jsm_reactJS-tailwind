21f820b5c918a8f86642ddf1ba6b1529be00d079
Ray Anthoney <rmaxey911@gmail.com>
2023-12-19 20:51:19 -0700

    query keys

--- jsm_react-tailwind-socialmedia/src/lib/react-query/queriesAndMutations.ts
+++ jsm_react-tailwind-socialmedia/src/lib/react-query/queriesAndMutations.ts
@@ -1,10 +1,16 @@
-import { 
-  useQuery, 
-  useMutation, 
-  useQueryClient, 
-  useInfiniteQuery, 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from "../appwrite/api";
+import {
+  createPost,
+  createUserAccount,
+  getRecentPosts,
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
 import { INewPost, INewUser } from "@/types";
 import { QUERY_KEYS } from "./queryKeys";
 
@@ -41,8 +47,7 @@ export const useCreatePost = () => {
 
 export const useGetRecentPosts = () => {
   return useQuery({
-    queryKey: [ QUERY_KEYS.GET_RECENT_POSTS],
-    queryFn: getRecentPosts
-  })
-}
-
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
+  });
+};
