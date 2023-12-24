c7ef2cbdfcc4b52b29eb479e10f0276b97aee9b9
Ray Anthoney <rmaxey911@gmail.com>
2023-12-23 22:16:00 -0700

    added PostCard page

---
+++ jsm_react-tailwind-socialmedia/src/components/shared/PostCard.tsx
@@ -0,0 +1,33 @@
+import { Models } from "appwrite";
+import { Link } from "react-router-dom";
+
+type PostCardProps = {
+  post: Models.Document;
+};
+
+const PostCard = ({ post }: PostCardProps) => {
+  return (
+    <div className="post-card">
+       <div className="flex-between">
+         <div className="flex items-center gap-3">
+         <Link to={`/profile/${post.creator.$id}`}>
+            <img
+              src={
+                post.creator?.imageUrl ||
+                "/assets/icons/profile-placeholder.svg"
+              }
+              alt="creator"
+              className="w-12 lg:h-12 rounded-full"
+            />
+          </Link>
+
+          <div className="flex flex-col">
+            <p>{post.creator.name}</p>
+          </div>
+        </div> 
+      </div> 
+    </div>
+  );
+};
+
+export default PostCard;
