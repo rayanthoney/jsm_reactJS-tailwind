c9c05bd6e184f0e4e2b7129178b2608656e33dd9
Ray Anthoney <rmaxey911@gmail.com>
2024-01-03 22:09:44 -0700

    creted the post stats page

--- jsm_react-tailwind-socialmedia/src/components/shared/PostStats.tsx
+++ jsm_react-tailwind-socialmedia/src/components/shared/PostStats.tsx
@@ -2,12 +2,8 @@ import { Models } from "appwrite";
 import React, { useState, useEffect } from "react";
 
 import { checkIsLiked } from "@/lib/utils";
-import {
-  useDeleteSavedPost,
-  useGetCurrentUser,
-  useLikePost,
-  useSavedPost,
-} from "@/lib/react-query/queriesAndMutations";
+import { useDeleteSavedPost, useGetCurrentUser, useLikePost, useSavePost } from "@/lib/react-query/queriesAndMutations";
+import Loader from "./Loader";
 
 type PostStatsProps = {
   post: Models.Document;
@@ -21,22 +17,21 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const [isSaved, setIsSaved] = useState(false);
 
   const { mutate: likePost } = useLikePost();
-  const { mutate: savePost } = useSavedPost();
-  const { mutate: deleteSavedPost } = useDeleteSavedPost();
+  const { mutate: savePost, isPending: isSavingPost } = useSavePost();
+  const { mutate: deleteSavedPost, isPending: isDeletingSaved } = useDeleteSavedPost();
 
   const { data: currentUser } = useGetCurrentUser();
 
   const savedPostRecord = currentUser?.save.find(
-    (record: Models.Document) => record.$id === post.$id
-  );
+    (record: Models.Document) => record.post.$id === post.$id);
 
-    // { saved: true } => !savedPostRecord => !false = true;
-    // 'test' => ! 'test' => !false = true;
-    // '' => !'' => !true = false;
+  // { saved: true } => !savedPostRecord => !false = true;
+  // 'test' => ! 'test' => !false = true;
+  // '' => !'' => !true = false;
 
   useEffect(() => {
-    setIsSaved(!!savedPostRecord)
-  }, [currentUser])
+    setIsSaved(!!savedPostRecord);
+  }, [currentUser]);
 
   const handleLikePost = (e: React.MouseEvent) => {
     e.stopPropagation();
@@ -57,16 +52,17 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
 
   const handleSavePost = (e: React.MouseEvent) => {
     e.stopPropagation();
-    
+
     if (savedPostRecord) {
       setIsSaved(false);
-      deleteSavedPost(savedPostRecord.$id);
+      return deleteSavedPost(savedPostRecord.$id);
     } else {
-      savePost({ postId: post.$id, userId });
+      savePost({ userId: userId, postId: post.$id });
       setIsSaved(true);
-    }
+    };
   };
 
+  // User Interface 
   return (
     <div className="flex justify-between items-center z-20">
       <div className="flex gap-2 mr-5">
@@ -86,14 +82,19 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
       </div>
 
       <div className="flex gap-2">
-        <img
-          src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
-          alt="like"
-          width={20}
-          height={20}
-          onClick={handleSavePost}
-          className="cursor-pointer"
-        />
+        {/* Loader for the save icon */}
+        {isSavingPost || isDeletingSaved ? (
+          <Loader />
+        ) : (
+          <img
+            src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
+            alt="like"
+            width={20}
+            height={20}
+            className="cursor-pointer"
+            onClick={(e) => handleSavePost(e)}
+          />
+        )}
       </div>
     </div>
   );
