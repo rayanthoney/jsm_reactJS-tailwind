681cbfd2f15883d7094c6d5a4722e36945e61073
Ray Anthoney <rmaxey911@gmail.com>
2024-01-03 22:07:33 -0700

    created the functionality for the like and saves

--- jsm_react-tailwind-socialmedia/src/components/shared/PostCard.tsx
+++ jsm_react-tailwind-socialmedia/src/components/shared/PostCard.tsx
@@ -49,8 +49,7 @@ const PostCard = ({ post }: PostCardProps) => {
 
         <Link
           to={`/update-post/${post.$id}`}
-          className={`${user.id !== post.creator.$id && "hidden"}`}
-        >
+          className={`${user.id !== post.creator.$id && "hidden"}`}>
           <img
             src="/assets/icons/edit.svg"
             alt="edit"
